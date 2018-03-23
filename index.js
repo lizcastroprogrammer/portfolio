81d1957e4b3247112bbb9adee4acf1955d0cef29
Elizabeth Castro <bookworm_428955@yahoo.com>
2018-03-23 17:23:43 +0000

    add href source to portfolio image

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var express = require("express"),
     app = express();
 
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
